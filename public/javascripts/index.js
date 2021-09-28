db2095ad81f4352cd7a1a94355a93bda04fce005
MIchael Huang <michaelnctu@gmail.com>
2021-09-28 11:54:55 +0800

    done

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -11,8 +11,11 @@ $(document).ready(function () {
   //   $(this).find('.dropdown').toggleClass('rotate');
   // });
   $('.sub-btn').click(function () {
+
     $(this).toggleClass('submenu-open').parent('div').siblings('div').children('a.submenu-open').removeClass('submenu-open');
+
     $(this).parent().toggleClass('submenu-open').children('div').slideToggle(500).end().siblings('.submenu-open').removeClass('submenu-open').children('div').slideUp(500);
+
     $(this).find('.dropdown').toggleClass('rotate');
     $('html, body').animate({
       scrollTop: (0),

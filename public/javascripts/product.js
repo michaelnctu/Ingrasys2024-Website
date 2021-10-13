8853e8dc8c6a57f61f013b83e756c42938660df2
MIchael Huang <michaelnctu@gmail.com>
2021-10-13 11:42:46 +0800

    done

--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -40,7 +40,7 @@ window.addEventListener('resize', slideImage);
 
 (function ($) {
   $.fn.responsiveTabs = function () {
-    this.addClass('responsive-tabs'), this.append($('<span class="dropdown-arrow"></span>')), this.on("click", "li.active, span.dropdown-arrow", function () {
+    this.addClass('responsive-tabs'), this.append($('<span class="dropdown-arrow"></span>')), this.on("click", "li.active, span.dropdown-arrow, .responsive-tabs", function () {
       this.toggleClass('open');
     }.bind(this)), this.on("click", "li > a:not(.active)", function () {
       this.removeClass("open");

2e22c2e05c15c6f2af22b841b0025a323f2b6403
MIchael Huang <michaelnctu@gmail.com>
2021-11-11 10:24:37 +0800

    done

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -220,21 +220,39 @@ function MultiCarousel($thisSlide, $maxSize) {
 // });
 
 
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    margin: 10, // 外距 10px
+    // nav: true, // 顯示點點
+    responsive: {
+      0: {
+        items: 1 // 螢幕大小為 0~600 顯示 1 個項目
+      },
+      600: {
+        items: 3 // 螢幕大小為 600~1000 顯示 3 個項目
+      },
+      1000: {
+        items: 3 // 螢幕大小為 1000 以上 顯示 5 個項目
+      }
+    }
+  });
+
+});
 
 
-$('.owl-carousel').owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: true,
+$(".owl-carousel").owlCarousel({
+  margin: 10, // 外距 10px
+  nav: false, // 顯示點點
+
   responsive: {
     0: {
-      items: 1
+      items: 1 // 螢幕大小為 0~600 顯示 1 個項目
     },
     600: {
-      items: 3
+      items: 3 // 螢幕大小為 600~1000 顯示 3 個項目
     },
     1000: {
-      items: 5
+      items: 4 // 螢幕大小為 1000 以上 顯示 5 個項目
     }
   }
-})
+});

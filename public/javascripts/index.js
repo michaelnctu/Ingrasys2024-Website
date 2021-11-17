1035bc0091e584d9ea074b642db92bf8a44dbd46
MIchael Huang <michaelnctu@gmail.com>
2021-11-17 16:44:56 +0800

    done

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -223,8 +223,8 @@ function MultiCarousel($thisSlide, $maxSize) {
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     margin: 10, // 外距 10px
-    nav: true, // 顯示點點
-    dots: true,
+    nav: true,
+    dots: true,// 顯示點點
     responsive: {
       0: {
         items: 1 // 螢幕大小為 0~600 顯示 1 個項目
